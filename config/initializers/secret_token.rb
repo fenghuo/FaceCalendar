f1bd8ce5bd5d310e105945bddb5673e81f68712c
Tianjiu <yintianjiu@gmail.com>
2013-10-21 21:00:23 -0700

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaceCalendar::Application.config.secret_token = '0db864351c04cb006793f5b823ad75ed99313679bdc1030ce75bb6a66b25f540594f92bf758cd7493a0159f8f50ea4789685a15b057e5497a1df3f96a45dc672'
