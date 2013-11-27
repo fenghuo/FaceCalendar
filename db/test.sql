4a110ed05c40dfe441569af8fd956dfae9d166d6
fenghuo <372615958@qq.com>
2013-11-26 23:22:20 -0800

    database

---
+++ db/test.sql
@@ -0,0 +1,7 @@
+use facecalendar;
+#call groups_findGroupWithName(1);
+#call event_getGroup(1,"2013-1-1","2013-12-12");
+#call event_getPrivate(1,"2013-1-1","2013-12-12");
+#call event_getAll(31111,"2013-1-1","2013-12-12");
+#call login_check("tsfsdf","sdfsf",@s);
+
