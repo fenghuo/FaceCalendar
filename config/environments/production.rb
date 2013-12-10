93702195382dd0e681a3924281b72b0dd8f1fa6c
myb800 <myb800@gmail.com>
2013-12-09 18:30:52 -0800

    scroll

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ FaceCalendar::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
-  config.cache_store = :memory_store
+  config.cache_store = :dalli_store
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = true #true
   config.action_controller.perform_caching = true
